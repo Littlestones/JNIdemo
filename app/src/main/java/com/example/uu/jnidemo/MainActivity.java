270112ff1df4d6acc3ffaffe583594a1ea9b8d2a
uu <897309731@qq.com>
2016-08-15 20:05:41 +0800

    demo

---
+++ app/src/main/java/com/example/uu/jnidemo/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.uu.jnidemo;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+	static{
+		System.loadLibrary("jnidemo");
+	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(this, "3+5的和为" + add(3, 5), 0).show();
+    }
+
+
+
+    public native int add(int i, int j);
+
+}
